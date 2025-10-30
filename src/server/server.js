ff1af590bd585a0edf40b91d21676116a6bd9d65
Juan Carlos Pérez González <jcarlos@lnxd-i2e31.teis.intranet>
2025-10-30 09:53:39 +0100

    poner el tipo usuario

---
+++ src/server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;  // Use PORT from environment or default to 5000
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Verificar variable
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
+/// Conexión a MongoDB 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Could not connect to MongoDB:", err));
+
+
+//Iniciar el servidor Express en el puerto especificado
+app.listen(PORT, () => {
+  console.log(`Server Express está corriendo en el puerto: ${PORT}`);
+});
